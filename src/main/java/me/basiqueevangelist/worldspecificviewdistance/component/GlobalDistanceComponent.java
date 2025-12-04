325ca427a7558cba0938d541bc4f384059b763cc
Wasinthorn Suksri <wasinthorn.suksri@gmail.com>
2025-12-04 22:46:41 +0700

    Update to 1.21.10 (#7)
    * chore: update to 1.21.10
    
    * fix: jar building
    
    * fix: injection index
    
    * ci: update deps

--- src/main/java/me/basiqueevangelist/worldspecificviewdistance/component/GlobalDistanceComponent.java
+++ src/main/java/me/basiqueevangelist/worldspecificviewdistance/component/GlobalDistanceComponent.java
@@ -1,7 +1,8 @@
 package me.basiqueevangelist.worldspecificviewdistance.component;
 
-import net.minecraft.nbt.NbtCompound;
-import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.storage.ReadView;
+import net.minecraft.storage.WriteView;
+
 import org.ladysnake.cca.api.v3.component.Component;
 
 public class GlobalDistanceComponent implements Component {
@@ -9,14 +10,14 @@ public class GlobalDistanceComponent implements Component {
     public int globalSimulationDistance = 0;
 
     @Override
-    public void readFromNbt(NbtCompound tag, RegistryWrapper.WrapperLookup registryLookup) {
-        tag.getInt("GlobalViewDistance").ifPresent(integer -> this.globalViewDistance = integer);
-        tag.getInt("GlobalSimulationDistance").ifPresent(integer -> this.globalSimulationDistance = integer);
+    public void readData(ReadView readView) {
+        readView.getOptionalInt("GlobalViewDistance").ifPresent(integer -> this.globalViewDistance = integer);
+        readView.getOptionalInt("GlobalSimulationDistance").ifPresent(integer -> this.globalSimulationDistance = integer);
     }
 
     @Override
-    public void writeToNbt(NbtCompound tag, RegistryWrapper.WrapperLookup registryLookup) {
-        tag.putInt("GlobalViewDistance", globalViewDistance);
-        tag.putInt("GlobalSimulationDistance", globalSimulationDistance);
+    public void writeData(WriteView writeView) {
+        writeView.putInt("GlobalViewDistance", globalViewDistance);
+        writeView.putInt("GlobalSimulationDistance", globalSimulationDistance);
     }
 }
